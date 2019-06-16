e31ccbb79c6cc06fcf0a1640b524817bfe503ab9
Gulleik <gulleik.olsen@gmail.com>
2019-06-16 13:04:51 +0200

    =lagt til vegger

---
+++ Source/Forsok2/Opendoor.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Opendoor.h"
+
+// Sets default values for this component's properties
+UOpendoor::UOpendoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpendoor::BeginPlay()
+{
+	Super::BeginPlay();
+	AActor* Door = GetOwner();
+
+	FRotator rotasjon = FRotator(0.f, 60.f, 0.f);
+	
+	Door->SetActorRotation(rotasjon);
+	// ...
+	
+}
+
+
+// Called every frame
+void UOpendoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
